9d5a7f53bf69f3a5ed56bb015d8d3a2df733d888
leslieyi <leslieyi411@gmail.com>
2021-10-01 15:03:03 -0400

    so much reading

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import reducer from "./features/books/booksSlice";
+import rootReducer from "./reducer";
 
 const store = createStore(
-  reducer,
+  rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-
+//By passing our rootReducer to the createStore method, the application maintains its initial state of { books: [], authors: [] }, just as it did when we had one reducer. From the application's perspective nothing has changed.
 ReactDOM.render(
   <Provider store={store}>
     <App />
